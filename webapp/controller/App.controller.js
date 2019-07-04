a40a7b6c5ae06f175324c45a16f19aa10802b5b0
francisco86 <grillo8585@gmail.com>
2019-07-04 18:20:53 +0000

    Carga Inicial desde controlador Json

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,44 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	//"jquery.sap.global",
+	"sap/ui/model/json/JSONModel"
+], function (Controller,JSONModel)
+{
 	"use strict";
 
-	return Controller.extend("ZNMSP.ZTEST_PEDIDOS.controller.App", {
-		onInit: function () {
-
-		}
+	var Controller =  Controller.extend("ZNMSP.ZTEST_PEDIDOS.controller.App",
+	{
+		onInit: function ()
+		{
+	//		var path = $.sap.getModulePath("mocks","/ZCABECERASet.json");
+		
+		    //var oModel = new JSONModel();
+		 
+		   
+			//var oModel = new JSONModel(sap.ui.require.toUrl("test/mock") + "/ZCABECERASet.json");
+			var oModel = new JSONModel($.sap.getModulePath("mocks") + "/ZCABECERASet.json");
+			this.getView().setModel(oModel);
+			console.log('$.sap.getModulePath("test/mock")');
+			console.log($.sap.getModulePath("test/mock"));
+			
+			console.log('sap.ui.require.toUrl("test/mock")');
+			console.log(sap.ui.require.toUrl("test/mock"));
+			console.log('sap.ui.require.toUrl("ZNMSP/mocks")');
+			console.log(sap.ui.require.toUrl("ZNMSP/mocks"));
+			
+			console.log('sap.ui.require.toUrl("mocks")');
+			console.log(sap.ui.require.toUrl("mocks"));
+			
+			console.log('$.sap.getModulePath("mocks")');
+			console.log($.sap.getModulePath("mocks"));
+			
+			console.log('sap.ui.require.toUrl("ZNMSP")');
+			console.log(sap.ui.require.toUrl("ZNMSP"));
+	     }
+		
+		
+		
 	});
+	
+	return Controller;
 });
